f77be76aa7ae43ab757c40d579fff2d71356e730
aragamix01 <aragamix01@gmail.com>
2017-10-06 15:50:51 +0700

    cc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,19 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { Content1Component } from './content1/content1.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    Content1Component
   ],
   imports: [
     CollapseModule.forRoot(),
     BsDropdownModule.forRoot(),
+    CarouselModule.forRoot(),
     FormsModule,
     BrowserModule
   ],
