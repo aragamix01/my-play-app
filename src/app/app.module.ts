edf07990c9772c3c6efebd632d7967280bca908d
aragamix01 <aragamix01@gmail.com>
2017-10-06 14:21:29 +0700

    bootstrap4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    BsDropdownModule.forRoot(),
     BrowserModule
   ],
   providers: [],
