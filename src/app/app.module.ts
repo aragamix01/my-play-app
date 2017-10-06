00d6da93339efa12c41375edf1049393804a1f47
aragamix01 <aragamix01@gmail.com>
2017-10-06 15:16:55 +0700

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
+    CollapseModule.forRoot(),
     BsDropdownModule.forRoot(),
+    FormsModule,
     BrowserModule
   ],
   providers: [],
