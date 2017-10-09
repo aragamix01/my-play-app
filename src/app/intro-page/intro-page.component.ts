ba8119f335d64c9f697a7fb77cb2a2f342694789
aragamix01 <aragamix01@gmail.com>
2017-10-09 15:39:28 +0700

    styling

--- src/app/intro-page/intro-page.component.ts
+++ src/app/intro-page/intro-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-intro-page',
@@ -7,9 +10,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroPageComponent implements OnInit {
   isIntro = false;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
